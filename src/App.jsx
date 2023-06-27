12dded4afbba3c1f7081c88ae392cdcf209254e5
samuel0273 <samuelgarciam03@gmail.com>
2023-06-27 15:34:45 -0500

    children

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
+import Main from './layouts/Main'
 import Index from './pages'
 import './index.css'
 
 function App() {
  
   return (
-    <div className=" flex flex-col min-h-screen w-full justify-between items-center bg-[#EBEBEB] m-auto md:px-8">
-    <NavBar />
-    <Index/>
-    <Footer />
-  </div>
+    <Main>
+      <Index />
+    </Main>
   )
 }
 
