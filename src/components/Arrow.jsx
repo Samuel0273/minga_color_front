9f971fa955d4e26fac63c4f45e87454a167f4562
samuel0273 <samuelgarciam03@gmail.com>
2023-06-27 21:45:23 -0500

    carrousel

---
+++ src/components/Arrow.jsx
@@ -0,0 +1,5 @@
+export default function Arrow({icon}) {
+  return (
+    <img className="bg-[#EBEBEB] w-6 rounded-lg p-1" src={icon} alt="flecha" />
+  )
+}
--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,14 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Arrow from "./Arrow";
+
 export default function Carrousel() {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/categories")
+      .then(res => setCategories(res.data.response))
+      .catch(err => console.log(err));
+  }, []);
+  let [counter,setCounter] = useState(0)
+    let next = ()=> (counter!==categories.length-1) ? setCounter(counter+1) : setCounter(0)
+    let prev = ()=> (counter!==0) ? setCounter(counter-1) : setCounter(categories.length-1)
+  const icon_left = "/src/assets/img/flecha-left.png";
+  const icon_right = "/src/assets/img/flecha.png";
+
   return (
     <div className="bg-gradient-to-r from-[#F9A8D4] to-[#F472B6] items-center justify-evenly rounded-lg mt-5 hidden px-5 md:flex md:h-40 md:w-full ">
-    <img className="bg-[#EBEBEB] w-6 rounded-lg p-1" src="/src/assets/img/flecha-left.png" alt="flecha" />
-      <img className="h-56 lg:h-64 self-end md:h-48" src="/src/assets/img/image2.png" alt="image 2" />
-      <img className="object-cover w-40 h-56 mx-10 mb-12 self-end rounded-lg md:w-32 md:h-48" src="/src/assets/img/image3.png" alt="imagen 3" />
+      <button className="button" onClick={prev}>
+      <Arrow icon={icon_left} alt="Previous" />
+      </button>
+      <img className="h-56 lg:h-64 self-end md:h-48" src={categories[counter]?.character_photo} alt="image 2" />
+      <img className="object-cover w-40 h-56 mx-10 mb-12 self-end rounded-lg md:w-32 md:h-48" src={categories[counter]?.cover_photo} alt="imagen 3" />
       <div className="text-l p-2 text-white sm:w-10/12 xl:w-6/12 xl:px-10">
-        <h3 className="text-2xl lg:text-3xl">Shonen</h3>
-        <p className="text-xs xl:text-sm lg:text-base">Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.</p>
+        <h3 className="text-2xl lg:text-3xl">{categories[counter]?.name}</h3>
+        <p className="text-xs xl:text-sm lg:text-base">{categories[counter]?.description}</p>
       </div>
-      <img className="bg-[#EBEBEB] w-6 rounded-lg p-1" src="/src/assets/img/flecha.png" alt="" />
+      <button className="button" onClick={next}>
+      <Arrow icon={icon_right} alt="Next" />
+      </button>
     </div>
-  )
+  );
 }
